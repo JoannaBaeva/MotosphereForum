2ae3b0f9f2c340d2e2d86babf6c0a107b6bb4e69
Joanna-Sofia Baeva <joannasofia7@gmail.com>
2025-05-23 01:12:39 +0300

    Fixed broken migrations

---
+++ MotorcycleForum/MotorcycleForum.Data/Migrations/20250522221051_InitPostgres.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MotorcycleForum.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MarketplaceListingImages",
+                keyColumn: "ImageId",
+                keyValue: new Guid("09eaefbb-6878-499a-806c-d7ff978d710e"));
+
+            migrationBuilder.DeleteData(
+                table: "MarketplaceListingImages",
+                keyColumn: "ImageId",
+                keyValue: new Guid("5b5c2e5c-e6b5-4624-baf8-6016a15acfe2"));
+
+            migrationBuilder.DeleteData(
+                table: "MarketplaceListingImages",
+                keyColumn: "ImageId",
+                keyValue: new Guid("bfaa5a5f-b39b-43b4-91e6-bf47da7bb418"));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("0ab81baf-1cdd-42cd-8d11-391f5118558e"),
+                column: "RegistrationDate",
+                value: new DateTime(2025, 5, 22, 22, 10, 50, 307, DateTimeKind.Utc).AddTicks(4425));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("f23a5f6d-1c7b-4a5b-97eb-08dbf6a6c3f8"),
+                column: "RegistrationDate",
+                value: new DateTime(2025, 5, 22, 22, 10, 50, 307, DateTimeKind.Utc).AddTicks(4295));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: new Guid("be4ccd71-8576-4378-8b7f-d943f17d19bb"),
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 22, 22, 10, 50, 307, DateTimeKind.Utc).AddTicks(4776));
+
+            migrationBuilder.UpdateData(
+                table: "ForumPosts",
+                keyColumn: "ForumPostId",
+                keyValue: new Guid("c6e5b16e-53f5-41c9-87cd-66da7a096b4a"),
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 22, 22, 10, 50, 307, DateTimeKind.Utc).AddTicks(4754));
+
+            migrationBuilder.InsertData(
+                table: "MarketplaceListingImages",
+                columns: new[] { "ImageId", "ImageUrl", "ListingId" },
+                values: new object[,]
+                {
+                    { new Guid("5c21520e-f319-4981-b9ae-80459c97872c"), "https://motosphere-images.s3.eu-north-1.amazonaws.com/marketplace/seed-images/michelin-tires-6-2.png", new Guid("efcc8a05-65a5-4cb3-859c-ccfc3e6a23bc") },
+                    { new Guid("773c3fca-dd0e-4290-9d2d-b7cd20fbeb64"), "https://motosphere-images.s3.eu-north-1.amazonaws.com/marketplace/seed-images/michelin-tires-6.png", new Guid("efcc8a05-65a5-4cb3-859c-ccfc3e6a23bc") },
+                    { new Guid("bb80c17e-4883-4d2f-a6e6-02cb9b601148"), "https://motosphere-images.s3.eu-north-1.amazonaws.com/marketplace/seed-images/Exaust.png", new Guid("7998740b-406d-4504-b9df-5f8aef508054") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MarketplaceListingImages",
+                keyColumn: "ImageId",
+                keyValue: new Guid("5c21520e-f319-4981-b9ae-80459c97872c"));
+
+            migrationBuilder.DeleteData(
+                table: "MarketplaceListingImages",
+                keyColumn: "ImageId",
+                keyValue: new Guid("773c3fca-dd0e-4290-9d2d-b7cd20fbeb64"));
+
+            migrationBuilder.DeleteData(
+                table: "MarketplaceListingImages",
+                keyColumn: "ImageId",
+                keyValue: new Guid("bb80c17e-4883-4d2f-a6e6-02cb9b601148"));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("0ab81baf-1cdd-42cd-8d11-391f5118558e"),
+                column: "RegistrationDate",
+                value: new DateTime(2025, 5, 22, 21, 27, 41, 551, DateTimeKind.Utc).AddTicks(9644));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("f23a5f6d-1c7b-4a5b-97eb-08dbf6a6c3f8"),
+                column: "RegistrationDate",
+                value: new DateTime(2025, 5, 22, 21, 27, 41, 551, DateTimeKind.Utc).AddTicks(9560));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: new Guid("be4ccd71-8576-4378-8b7f-d943f17d19bb"),
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 22, 21, 27, 41, 551, DateTimeKind.Utc).AddTicks(9888));
+
+            migrationBuilder.UpdateData(
+                table: "ForumPosts",
+                keyColumn: "ForumPostId",
+                keyValue: new Guid("c6e5b16e-53f5-41c9-87cd-66da7a096b4a"),
+                column: "CreatedDate",
+                value: new DateTime(2025, 5, 22, 21, 27, 41, 551, DateTimeKind.Utc).AddTicks(9869));
+
+            migrationBuilder.InsertData(
+                table: "MarketplaceListingImages",
+                columns: new[] { "ImageId", "ImageUrl", "ListingId" },
+                values: new object[,]
+                {
+                    { new Guid("09eaefbb-6878-499a-806c-d7ff978d710e"), "https://motosphere-images.s3.eu-north-1.amazonaws.com/marketplace/seed-images/michelin-tires-6-2.png", new Guid("efcc8a05-65a5-4cb3-859c-ccfc3e6a23bc") },
+                    { new Guid("5b5c2e5c-e6b5-4624-baf8-6016a15acfe2"), "https://motosphere-images.s3.eu-north-1.amazonaws.com/marketplace/seed-images/Exaust.png", new Guid("7998740b-406d-4504-b9df-5f8aef508054") },
+                    { new Guid("bfaa5a5f-b39b-43b4-91e6-bf47da7bb418"), "https://motosphere-images.s3.eu-north-1.amazonaws.com/marketplace/seed-images/michelin-tires-6.png", new Guid("efcc8a05-65a5-4cb3-859c-ccfc3e6a23bc") }
+                });
+        }
+    }
+}
